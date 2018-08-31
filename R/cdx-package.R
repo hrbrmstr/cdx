63437323f4e08e2cafec67a82def39c6a4b50ad2
boB Rudis <bob@rud.is>
2018-08-31 18:19:13 -0400

    R package repo initialization complete

---
+++ R/cdx-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/cdx>
+#' - BugReports: <https://gitlab.com/hrbrmstr/cdx/issues>
+#' 
+#' @md
+#' @name cdx
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
